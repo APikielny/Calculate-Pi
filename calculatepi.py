f9bc78423da3be9cc9e4a98b171f622b373c5c82
APikielny <apikielny@gmail.com>
2015-09-11 14:36:59 -0400

    Updated from Brython Server: September 11, 2015 2:36:59 PM EDT

--- calculatepi.py
+++ calculatepi.py
@@ -1,7 +1,7 @@
 """
 calculatepi.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Adam Pikielny
+Credit: Mr. Dennison helped with an error in my code.
 Assignment:
 
 Write and submit a Python program that computes an approximate value of π by calculating the following sum:
